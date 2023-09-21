cdb5c069ca77d16522c896d603d0aaa20b606652
Grace <gracenyangayi@gmail.com>
2023-09-21 16:30:15 +0300

    Update

--- src/App.js
+++ src/App.js
@@ -8,30 +8,46 @@ import { useState } from 'react';
 
 
 
+
 function App() {
-  const [column, setColumn] = useState([])
+  const columns=[
+    'Name',
+    'Country',
+    'Head Quaters',
+    
+  ]
+
   const [records, setRecords] = useState([])
 
   useEffect(() => {
     fetch('http://localhost:3000/Data.json')
     .then(res => res.json())
     .then(data => {
-      setColumn(Object.keys(data.airlines[0]))
       setRecords(data.airlines)
     })
 
   }, [])
+
+  useEffect(()=>{
+    console.log(columns)
+  },[columns])
   
   
   return (
     
     <div>
       <h1>Know your Airlines!!</h1>
+      
             <Table striped bordered hover>
                 <thead>
                 <tr>
                     
-                    {column.map((c, i) => (<th key={i}>{c}</th>))}
+                    {columns.map((column , i) => (
+                    <th key={i}>
+                      {column}
+                      </th>
+                      
+                    ))}
                 </tr>
                 </thead>
                 <tbody>
@@ -39,8 +55,8 @@ function App() {
                         records.map((record, i) => (
                             <tr key={i}>
                                 
-                                <td>{record.country}</td>
                                 <td>{record.name}</td>
+                                <td>{record.country}</td>
                                 <td>{record.head_quaters}</td>
                                 <td><Button>view</Button></td>
                             </tr>
