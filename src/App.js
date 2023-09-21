5ac07a7593710ca493a63fbf5a6856c057655dd0
Grace <gracenyangayi@gmail.com>
2023-09-21 14:35:28 +0300

    updates

--- src/App.js
+++ src/App.js
@@ -1,10 +1,57 @@
+import React from 'react';
+import Table from 'react-bootstrap/Table';
+import Button from 'react-bootstrap/Button';
+import { useEffect } from 'react';
+import { useState } from 'react';
 
-import Display from "./components/Display";
+
+
+
 
 function App() {
+  const [column, setColumn] = useState([])
+  const [records, setRecords] = useState([])
+
+  useEffect(() => {
+    fetch('http://localhost:3000/Data.json')
+    .then(res => res.json())
+    .then(data => {
+      setColumn(Object.keys(data.airlines[0]))
+      setRecords(data.airlines)
+    })
+
+  }, [])
+  
+  
   return (
     
-      <Display />
+    <div>
+      <h1>Know your Airlines!!</h1>
+            <Table striped bordered hover>
+                <thead>
+                <tr>
+                    
+                    {column.map((c, i) => (<th key={i}>{c}</th>))}
+                </tr>
+                </thead>
+                <tbody>
+                    {
+                        records.map((record, i) => (
+                            <tr key={i}>
+                                
+                                <td>{record.country}</td>
+                                <td>{record.name}</td>
+                                <td>{record.head_quaters}</td>
+                                <td><Button>view</Button></td>
+                            </tr>
+                        ))
+
+
+                        
+                    }
+                </tbody>
+            </Table>
+        </div>
     
   );
 }
--- src/components/Display.js
+++ src/components/Display.js
@@ -1,11 +1,43 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-function DisplayList(){
+function Display(){
+        const [column, setColumn] = useState([])
+        const [records, setRecords] = useState([])
+       
+    useEffect(() => {
+       fetch('http://localhost:3000/data.json')
+       .then(response => response.json())
+       .then(data => {
+        setColumn(Object.keys(data.airlines[0]))
+        setRecords(data.airlines)
+       })
+    }, [])
     return (
         <div>
-            <h1>List</h1>
+            <table>
+                <thead>
+                <tr>
+                    
+                    {column.map((c,i) => (<th key={i}>{c}</th>))}
+                </tr>
+                </thead>
+                <tbody>
+                    {
+                        records.map((record, i) => (
+                            <tr key={i}>
+                                <td>{record.id}</td>
+                                <td>{record.name}</td>
+                                <td>{record.head_quaters}</td>
+                            </tr>
+                        ))
+
+
+                        
+                    }
+                </tbody>
+            </table>
         </div>
     )
 }
 
-export default DisplayList;
+export default Display;
